458d26833475675db1fe94345cb4caa5a998980c
supri_rmvse5l <supriya28.mane@gmail.com>
2024-07-09 11:46:31 +0530

    file added

--- src/SamplePackage/Hello.java
+++ src/SamplePackage/Hello.java
@@ -1,2 +1,7 @@
-package SamplePackage;public class Hello {
+package SamplePackage;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
